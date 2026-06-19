721ec1fb8f0fd393602faf1ac1ddd8243d1cb8df
thunder1210 <118947074+thunder1210@users.noreply.github.com>
2026-06-19 18:47:57 +0800

    Add: implement-prefix-tree - submission-15

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-15.java
@@ -0,0 +1,54 @@
+class TrieNode {
+    TrieNode[] children;
+    boolean isEndWord;
+
+    public TrieNode() {
+        children = new TrieNode[26];
+        isEndWord = false;
+    }
+}
+
+class PrefixTree {
+
+    private TrieNode root;
+
+    public PrefixTree() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode cur = root;
+        for (char c : word.toCharArray()) {
+            int i = c - 'a';
+            if (cur.children[i] == null) {
+                cur.children[i] = new TrieNode();
+            }
+            cur = cur.children[i];
+        }
+        cur.isEndWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode cur = root;
+        for (char c : word.toCharArray()) {
+            int i = c - 'a';
+            if (cur.children[i] == null) {
+                return false;
+            }
+            cur = cur.children[i];
+        }
+        return cur.isEndWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode cur = root;
+        for (char c : prefix.toCharArray()) {
+            int i = c - 'a';
+            if (cur.children[i] == null) {
+                return false;
+            }
+            cur = cur.children[i];
+        }
+        return true;
+    }
+}
