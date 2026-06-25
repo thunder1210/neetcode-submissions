3567c60ff271cb5ffd05731b99205b62bb5e951c
thunder1210 <118947074+thunder1210@users.noreply.github.com>
2026-06-25 16:54:44 +0800

    Add: longest-substring-without-duplicates - submission-88

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-88.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        int max = 0;
+        int left = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+            if (map.containsKey(c)) {
+                left = Math.max(left, map.get(c) + 1);
+            } 
+            map.put(c, right);
+            max = Math.max(max, right - left + 1);
+        }
+        return max;
+    }
+}
