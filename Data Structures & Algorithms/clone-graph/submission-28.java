d1c0ff7a37ac6bb04d539ca5186bb7b4a6ca6714
thunder1210 <118947074+thunder1210@users.noreply.github.com>
2026-07-15 10:20:51 +0800

    Add: clone-graph - submission-28

---
+++ Data Structures & Algorithms/clone-graph/submission-28.java
@@ -0,0 +1,41 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        if (node == null) return null; 
+        Map<Node, Node> map = new HashMap<>();
+        Queue<Node> q = new LinkedList<>();
+        map.put(node, new Node(node.val));
+        q.offer(node);
+
+        while (!q.isEmpty()) {
+            Node cur = q.poll();
+            for (Node nei : cur.neighbors) {
+                if (!map.containsKey(nei)) {
+                    map.put(nei, new Node(nei.val));
+                    q.offer(nei);
+                }
+                map.get(cur).neighbors.add(map.get(nei));
+            }
+        }
+        return map.get(node);
+    }
+}
